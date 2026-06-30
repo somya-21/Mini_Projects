aef460ec77c28b4ac156a4ca9297613f887ea70c
somya-21 <123484441+somya-21@users.noreply.github.com>
2026-06-30 05:24:49 +0000

    Project1

---
+++ Javascript/colorChanger.js
@@ -0,0 +1,39 @@
+// let buttons=document.querySelectorAll('.button');
+// let body=document.querySelector('body');
+// //dom
+
+// //loops
+// buttons.forEach(
+//     (button)=>
+//     {
+        
+//             button.addEventListener('click',(e)=>{
+//                 if(e.target.value==='Red')
+//                 {
+//                     document.body.style.backgroundColor='red';
+//                 }
+//                 else if(e.target.value==='Blue')
+//                 {
+//                    body.style.backgroundColor='blue';
+//                 }
+//                 else if(e.target.value==='Green')
+//                 {
+//                     document.body.style.backgroundColor='green';
+//                 }   
+//                  else if(e.target.value==='Pink')
+//                 {
+//                     document.body.style.backgroundColor='pink';
+//                 }  
+//                  else if(e.target.value==='Purple')
+//                 {
+//                     document.body.style.backgroundColor='purple';
+//                 }  
+//                  else if(e.target.id==='Black')
+//                 {
+//                     document.body.style.backgroundColor='black';
+//                 }  
+//             })
+        
+//     }
+// )
+
